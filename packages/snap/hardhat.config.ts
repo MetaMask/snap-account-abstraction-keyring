59ae10efd228fe49cd0abab0579fa220c8800a5b
Monte Lai <monte.lai@consensys.net>
2024-01-11 19:49:57 +0800

    feat: add hardhat and openzeppelin

---
+++ packages/snap/hardhat.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-unassigned-import */
+import type { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@typechain/hardhat';
+import '@nomicfoundation/hardhat-ethers';
+import '@nomicfoundation/hardhat-chai-matchers';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.19',
+  typechain: {
+    outDir: 'src/types',
+    target: 'ethers-v6',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false, // defaults to false
+  },
+};
+
+export default config;
